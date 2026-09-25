4c4b418958f98502df69532a32c788ae20585366
Msanfer1403 <msanfer1403@ieszaidinvergeles.org>
2026-09-25 11:37:25 +0000

    Added a small code to display de data saved on variables

---
+++ task2.py
@@ -0,0 +1,15 @@
+# Task2 Variables .py
+
+x = "Maximo Sanchez"
+a = 20
+
+h = 1.78
+
+s = True
+
+print(f"{x} | {a} | {h} | {s}")
+
+student_name = x
+student_age = a
+student_height = h
+student_enrolled = s
